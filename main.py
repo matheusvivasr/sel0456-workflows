0f3d41de7b6c885088a030655980358d08cca699
matheusvivasr <vivas.matheus@usp.br>
2022-11-12 02:12:05 -0300

    test files and small modifications added

--- main.py
+++ main.py
@@ -1,40 +1,40 @@
 import funcs as f
 import pandas as pd
 
-def tratamento(num):
-    #simplifica o cálculo de cada linha.
+def tratamento(num):    #simplifica o cálculo de cada linha.
     ft = f.fatorial(num)
     fb = f.fibonacci(num)
     return [int(num),ft,fb]
 
 def comparar(esse:(pd.DataFrame), aquele:(pd.DataFrame)):
-    # Compara os dados e devolve uma lista com 
-    # os números para os quais os valores divergiram.
+# Compara os dados e devolve uma lista com os números para os quais os valores divergiram.
     wLines = []     
     if not esse.equals(aquele):
-        bino = esse.eq(aquele)
+        bino = esse.eq(aquele)  
+        # Retorna uma matriz binária em que True são valores de mesmo indice idênticos.
+
         for i in range(bino.shape[0]):
-            if not (bino.iloc[i].iat[1] and bino.iloc[i].iat[2]): wLines.append(esse["#"].iat[i])
+            if not (bino.iloc[i].iat[1] and bino.iloc[i].iat[2]): 
+            # NAND para conferir se nem o fatorial nem o fibonacci estão errados.
+                wLines.append(esse["#"].iat[i]) 
+                # escreve para quais valores a resposta esta errada.
     return wLines
 
 
-fileName = "./test.data"    #Arquivo a ser testado.
+fileName = "./test2.data"    #Arquivo a ser testado.
 
 
 # Transforma a entrada em pd.DataFrame.
 inputFile = pd.DataFrame([linhas.strip().split() for linhas in open(fileName).readlines()])
-if inputFile.loc[0].at[0].isnumeric():
-    testFile = pd.DataFrame(inputFile.values,columns=["#","fact","fib"])
-else:
-    testFile = pd.DataFrame(inputFile[1:].values,columns=inputFile.iloc[0].values)
+testFile = pd.DataFrame(inputFile[1:].values,columns=inputFile.iloc[0].values)
 
 
 # Faz o arquivo de saída calculado com a funçao 'tratamento' definida acima.
 outText = pd.DataFrame([tratamento(n) for n in testFile["#"].values],columns=testFile.columns.values)
 with open("./outFile.data","w") as testOut:
-    testOut.write(' '.join(["#","fact","fib"]))
+    testOut.write(' '.join(inputFile.iloc[0].values))
     testOut.write('\n')
-    for i in range(outText.shape[0]):   #escreve o arquivo de acordo com o modelo esperado.
+    for i in range(outText.shape[0]):   #escreve o arquivo de saída em acordo com o modelo esperado.
         for j in range(outText.shape[1]):
             testOut.write(str(outText.iloc[i].values[j]))
             if j < outText.shape[1]-1: testOut.write(' ')
@@ -48,4 +48,4 @@ outFile = pd.DataFrame(outputFile[1:].values,columns=outputFile.iloc[0].values)
 linhas_erradas = comparar(testFile,outFile) #funçao definida no começo do programa.
 
 if linhas_erradas==[]:print("Programa sem erros")
-else:print("Os valores foram errados para as linhas: ",", ".join(linhas_erradas))
+else:print("Os valores foram errados para os valores:",", ".join(linhas_erradas))
